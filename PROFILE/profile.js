1fc979c1aad57c238bd3206c8d4df29d6ce9eba4
kaloyyyyy10 <carlopmendoza3@gmail.com>
2024-12-09 13:36:02 +0800

    ayaw kona

---
+++ PROFILE/profile.js
@@ -0,0 +1,10 @@
+function toggleDropdown(dropdownId) {
+    const dropdownContent = document.getElementById(dropdownId + '-dropdown');
+    dropdownContent.classList.toggle('show');
+  }
+
+  function toggleDropdown(id) {
+    var dropdownContent = document.getElementById(id + '-dropdown');
+    dropdownContent.classList.toggle('show');
+  }
+
